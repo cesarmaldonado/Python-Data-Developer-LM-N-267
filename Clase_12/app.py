c3a6b05058460640c97d0811de8403af186f8439
cesarmaldon <maldonado_cesar85@hotmail.com>
2021-04-14 21:50:32 -0300

    Implementando ruta JSON response

---
+++ Clase_12/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_flask():
+    return 'Hola desde Flask :D'
+@app.route('/users')
+def usersTwitter():
+    users = [
+        {'name' :'smessina_'},
+        {'name' :'eanttech'},
+        {'name' :'TinchoLutter'},
+        {'name' :'bitcoinArg'}
+        ]
+    response = app.response_class(response = json.dumps(users), status = 200, mimetype = 'application/json')
+    return response
+
+if __name__ == '__main__':
+    app.run( port = 3030)
